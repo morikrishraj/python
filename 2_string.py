f24196120f5bf27e463cb8e6e810e33d73aa4d37
morikrishraj <morikrishraj6728@gmail.com>
2026-01-07 11:51:59 +0530

    Add files via upload

---
+++ 2_string.py
@@ -0,0 +1,15 @@
+name="THE EASYLEARN ACADEMY"
+city="Bhavnager"
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[0:3])
+print(name[4:13])
+print(name[4:])
+print(name+city)
+print(city*5)
+city="Baroda"
+print(city)
+print('good bye')
