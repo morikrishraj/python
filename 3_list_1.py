35ee154ba8f8d06c44e71eb86265a7790aa8df18
morikrishraj <morikrishraj6728@gmail.com>
2026-01-07 11:52:46 +0530

    Add files via upload

---
+++ 3_list_1.py
@@ -0,0 +1,12 @@
+#examle of list 
+box=['mouse',100,3.14,True,None]
+marks=[50,75,85,95,65]
+print(box)
+print(marks)
+#get 1st item in box
+print(box[0])
+print(box[0:2])
+print(box[2:])
+print(marks*2)
+print(box+marks)
+
