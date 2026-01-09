f2d182a6bb74526534d29b5338af6a64649c8111
morikrishraj <morikrishraj6728@gmail.com>
2026-01-09 12:01:23 +0530

    Add files via upload

---
+++ 14_logical_operator.py
@@ -0,0 +1,17 @@
+#logical operator
+num1=100
+num2=200
+num3=300
+#And or or
+result=num1<num2 and num2<num3
+print(f"{result}={num1}<{num2} and {num2}<{num3}")
+result=num1<num2 and num2>num3
+print(f"{result}={num1}<{num2} and {num2}>{num3}")
+result=num1<num2 or num2>num3
+print(f"{result}={num1}<{num2} or {num2}>{num3}")
+result=num1>num2 or num2>num3
+print(f"{result}={num1}>{num2} or {num2}>{num3}")
+result=not(num1>num2 or num2>num3)
+print(f"{result}=not{num1}<{num2} or {num2}<{num3}")
+
+
