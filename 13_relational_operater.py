b038316fcb63c9a961909b2da6cb67c0c35b68a9
morikrishraj <morikrishraj6728@gmail.com>
2026-01-09 12:00:57 +0530

    Add files via upload

---
+++ 13_relational_operater.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter value for num1"))
+num2=int(input("Enter value for num2"))
+result=num1==num2
+print(f"{result}={num1}=={num2}")
+result=num1!=num2
+print(f"{result}={num1}!={num2}")
+result=num1<num2
+print(f"{result}={num1}<{num2}")
+result=num1>num2
+print(f"{result}={num1}>{num2}")
+result=num1<=num2
+print(f"{result}={num1}<={num2}")
+result=num1>=num2
+print(f"{result}={num1}>={num2}")
