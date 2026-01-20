bd4c62e956f147182ca9681af238108d9bf80514
morikrishraj <morikrishraj6728@gmail.com>
2026-01-20 11:53:13 +0530

    Create 3_if_day_num.py

---
+++ exercise/3_if_day_num.py
@@ -0,0 +1,16 @@
+day=int(input("Enter day number"))
+if day==1:
+    print("monday")
+if day==2:
+    print("tuesday")
+if day==3:
+    print("wenasday")
+if day==4:
+    print("thersday")
+if day==5:
+    print("friday")
+if day==6:
+    print("saterday")
+if day==7:
+    print("sunday")
+    
