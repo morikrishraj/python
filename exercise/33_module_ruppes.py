d5039fe33b39b888525c5573d4048d7569731f2c
morikrishraj <morikrishraj6728@gmail.com>
2026-02-21 11:54:34 +0530

    Add files via upload

---
+++ exercise/33_module_ruppes.py
@@ -0,0 +1,8 @@
+import currency
+
+amount = 1000  # Rupees
+
+print("Dollar:", currency.toDollar(amount))
+print("Euro:", currency.toEuro(amount))
+print("Pound:", currency.toPound(amount))
+print("Yen:", currency.ToYen(amount))
